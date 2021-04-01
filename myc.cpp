4aa01c68101df90a187acf36f144c878e62a3d9f
imthewind <81433966+imthewind@users.noreply.github.com>
2021-04-01 22:17:07 +0800

    Update myc.cpp

--- myc.cpp
+++ myc.cpp
@@ -7,6 +7,10 @@ Stu()
 {
 cout<<"构造函数的调用"<<endl;
 }
+  ~stu()
+  {
+   cout<<"析构函数的调用“<<endl; 
+  }
 private:
 int m_age;
 }
@@ -14,6 +18,7 @@ int m_age;
 int main()
 {
 Stu p1;
+  free(p1);
 system("pause");
 return 0;
 }
